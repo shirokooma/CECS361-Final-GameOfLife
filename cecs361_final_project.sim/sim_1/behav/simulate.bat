6fe34d601391fa63d9e025c2f8e1e999e1e67ea2
Evan Nguyen <59784844+nguyenevan42@users.noreply.github.com>
2021-10-28 20:27:43 -0700

    Delete cecs361_final_project.sim/sim_1/behav directory

--- cecs361_final_project.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.4\\bin
-call %xv_path%/xsim clk_div_tb_behav -key {Behavioral:sim_1:Functional:clk_div_tb} -tclbatch clk_div_tb.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
