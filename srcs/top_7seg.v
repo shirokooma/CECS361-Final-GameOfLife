a376e454df93c98a718715ac1837f3ba8aa0b429
Everette Esguerra <35823013+ShirouKuma@users.noreply.github.com>
2021-12-05 06:33:27 -0800

    update top 7 seg

--- srcs/top_7seg.v
+++ srcs/top_7seg.v
@@ -23,16 +23,25 @@
 module top_7seg(
     input in1,
     input in0,
+    input [1:0] pattern_a, // Glider
+    input [1:0] pattern_b, // Blinker
+    input [1:0] pattern_c, // Beacon
+    input [1:0] pattern_d,
     output[7:0] cathode,
     output [7:0] anode
     );
     
     assign anode = 8'b1111_1110;
     wire [3:0] Q;
+    wire [1:0] out_pattern;
     
     BCD_to_7seg
         BCD(.Q(Q),.cathode(cathode));
     
+    pattern_sel
+        sel(.in1(in1),.in0(in0),.pattern_a(pattern_a),.pattern_b(pattern_b),.pattern_c(pattern_c),.pattern_d(pattern_d),.Q(Q),.out_pattern(out_pattern));
+endmodule
+
     pattern_sel
         sel(.in1(in1),.in0(in0),.Q(Q));
 endmodule
